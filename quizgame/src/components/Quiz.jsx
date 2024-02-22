4d561547e78b92fd9f793395117bf2a87a5e96dc
myky034 <vumyky.034@gmail.com>
2024-02-22 08:36:20 +0700

    Adding list questions

--- quizgame/src/components/Quiz.jsx
+++ quizgame/src/components/Quiz.jsx
@@ -1,3 +1,51 @@
+import { useState } from "react";
+import quizComplete from "../assets/quiz-complete.png";
+
+import QUESTION from "../questions.js";
+
 export default function Quiz() {
-  return <p>Currently active Question</p>;
+  const [userAnswer, setUserAnswer] = useState([]);
+
+  const activeQuestionIndex = userAnswer.length;
+  const quizIsComplete = activeQuestionIndex === QUESTION.length;
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswer((prevUserAnswer) => {
+      return [...prevUserAnswer, selectedAnswer];
+    });
+  }
+
+  if (quizIsComplete) {
+    return (
+      <div id='summary'>
+        <img
+          src={quizComplete}
+          alt='Quiz Completed'
+        />
+        <h2>Quiz Completed !</h2>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = [...QUESTION[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
+  return (
+    <div id='quiz'>
+      <div id='question'>
+        <h2>{QUESTION[activeQuestionIndex].text}</h2>
+        <ul id='answers'>
+          {shuffledAnswers.map((answer) => (
+            <li
+              key={answer}
+              className='answer'>
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
